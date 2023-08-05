289cf140f4c787f0741f951e67149a7a9f228bdf
hikile <140878313+hikile@users.noreply.github.com>
2023-08-06 01:51:19 +0700

    btvn1

--- bai3.java
+++ bai3.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class bai3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số nguyên dương n: ");
-        int n = scanner.nextInt();
+        int n;
+        do{
+            System.out.print("Nhập số nguyên dương n: ");
+             n = new Scanner(System.in).nextInt();
+        }while(n<=0);
+
         boolean check=true;
         for(int i=2 ; i*i<=n ; i++){
             if(n%i==0){
@@ -13,7 +17,7 @@ public class bai3 {
             }
         }
         System.out.println("Kết quả: " );
-        if(check){
+        if(check && n>=2){
             System.out.println(n + " là sô nguyên tố " );
         }
         else{
